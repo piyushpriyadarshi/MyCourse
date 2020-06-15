85c840df99df025842bbbe4049f04e36ff58cbd3
Piyush Priyadarshi <priyadarship85@gmail.com>
2020-06-15 19:55:11 +0530

    Completed Asssignment 3

--- udemy/HibernateCourseByReshmaBidikar/HibernateFromScratch/src/main/java/com/piyush/hibernate/Test.java
+++ udemy/HibernateCourseByReshmaBidikar/HibernateFromScratch/src/main/java/com/piyush/hibernate/Test.java
@@ -1,8 +1,12 @@
 package com.piyush.hibernate;
 
 import com.piyush.hibernate.entity.Book;
+import com.piyush.hibernate.entity.Person;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -13,22 +17,50 @@ import org.hibernate.Transaction;
  */
 public class Test {
     public static void main(String[] args) {
-        Book book=new Book();
-        book.setBookAuthor("Piyush priyadarshi");
-        book.setBookName("My Journey2");
+//        Book book=new Book();
+//        book.setBookAuthor("Piyush priyadarshi");
+//        book.setBookName("My Journey2");
+//
+//
+//        Session session=HibernateUtil.getSession();
+//
+//        Transaction tx=session.beginTransaction();
+//        session.save(book);
+//
+//
+//        tx.commit();
+//
+//        HibernateUtil.closeSession(session);
+//
+//        HibernateUtil.closeSessionFactory();
 
+        /*
+
+
+        1) 'Mickey Mouse', 35, Male
+        2)  'Donald Duck', 30, Male
+        3) Daisy Duck 32, Female,
+        4) Peppa Pig 15, Female,
+        5) Suzie Sheep, 18, Female
+         */
+
+        List<Person> personList=new ArrayList<Person>();
+        personList.add(new Person("Mickey Mouse",35,"Male"));
+        personList.add(new Person("Donald Duck",30,"Male"));
+        personList.add(new Person("Daisy Duck",32,"Female"));
+        personList.add(new Person("Peppa Pig",15,"Female"));
+        personList.add(new Person("Suzie Sheep",18,"Female"));
 
         Session session=HibernateUtil.getSession();
-
         Transaction tx=session.beginTransaction();
-        session.save(book);
-
-
+        for (Person person:personList) {
+            session.persist(person);
+        }
         tx.commit();
-
         HibernateUtil.closeSession(session);
-
         HibernateUtil.closeSessionFactory();
+
+
     }
 
 }
---
+++ udemy/HibernateCourseByReshmaBidikar/HibernateFromScratch/src/main/java/com/piyush/hibernate/entity/Person.java
@@ -0,0 +1,74 @@
+package com.piyush.hibernate.entity;
+
+import javax.persistence.*;
+
+/**
+ * Created by Piyush Priyadarshi.
+ * User: @Piyush
+ * Date: 15-06-2020
+ * Time: 19:43
+ */
+@Entity
+@Table(name = "person")
+public class Person {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private Integer age;
+    private String gender;
+
+    public Person() {
+    }
+
+    public Person(Integer id, String name, Integer age, String gender) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    public Person(String name, Integer age, String gender) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public Person setId(Integer id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Person setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public Person setAge(Integer age) {
+        this.age = age;
+        return this;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public Person setGender(String gender) {
+        this.gender = gender;
+        return this;
+    }
+}
