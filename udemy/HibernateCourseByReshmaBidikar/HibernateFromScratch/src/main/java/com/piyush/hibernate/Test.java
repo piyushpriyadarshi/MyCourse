be9b13a0d99db3a5b6d767aa4615f753b4deade0
Piyush Priyadarshi <priyadarship85@gmail.com>
2020-06-16 11:07:51 +0530

    load vs get

--- udemy/HibernateCourseByReshmaBidikar/HibernateFromScratch/src/main/java/com/piyush/hibernate/Test.java
+++ udemy/HibernateCourseByReshmaBidikar/HibernateFromScratch/src/main/java/com/piyush/hibernate/Test.java
@@ -17,48 +17,21 @@ import java.util.List;
  */
 public class Test {
     public static void main(String[] args) {
-//        Book book=new Book();
-//        book.setBookAuthor("Piyush priyadarshi");
-//        book.setBookName("My Journey2");
-//
-//
-//        Session session=HibernateUtil.getSession();
-//
-//        Transaction tx=session.beginTransaction();
-//        session.save(book);
-//
-//
-//        tx.commit();
-//
-//        HibernateUtil.closeSession(session);
-//
-//        HibernateUtil.closeSessionFactory();
-
-        /*
-
-
-        1) 'Mickey Mouse', 35, Male
-        2)  'Donald Duck', 30, Male
-        3) Daisy Duck 32, Female,
-        4) Peppa Pig 15, Female,
-        5) Suzie Sheep, 18, Female
-         */
-
-        List<Person> personList=new ArrayList<Person>();
-        personList.add(new Person("Mickey Mouse",35,"Male"));
-        personList.add(new Person("Donald Duck",30,"Male"));
-        personList.add(new Person("Daisy Duck",32,"Female"));
-        personList.add(new Person("Peppa Pig",15,"Female"));
-        personList.add(new Person("Suzie Sheep",18,"Female"));
+    //Load vs Get
 
         Session session=HibernateUtil.getSession();
         Transaction tx=session.beginTransaction();
-        for (Person person:personList) {
-            session.persist(person);
-        }
+
+//        Person person=(Person) session.load(Person.class,10);
+//        System.out.println(person.getName());
+
+        Person person1=(Person) session.get(Person.class,10);
+        System.out.println(person1.getName());
+
         tx.commit();
         HibernateUtil.closeSession(session);
         HibernateUtil.closeSessionFactory();
+
 
 
     }
